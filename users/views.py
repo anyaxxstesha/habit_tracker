4749402d16a3a98fcbd257485b327385416b0b5f
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-08-26 21:59:03 +0300

    add user CRUD and configure user urls

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from users.models import User
+from users.serializers import UserSerializer
 
-# Create your views here.
+
+class UserListAPIView(ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDestroyAPIView(DestroyAPIView):
+    queryset = User.objects.all()
