5c5b3dc6732e43ebb906b1a223406e7088a2686c
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-08-27 22:23:12 +0300

    expand validators

--- habits/validators.py
+++ habits/validators.py
@@ -4,11 +4,18 @@ from rest_framework.serializers import ValidationError
 
 
 def pleasant_habit_or_reward(value):
-    """Проверяет, что в модели заполнено либо поле associated_habit, либо reward"""
+    """
+    Проверяет, что в модели полезной привычки заполнено либо поле associated_habit, либо reward.
+    В модели приятной привычки не могут быть заполнены поля associated_habit и reward
+    """
     reward = value.get('reward')
     associated_habit = value.get('associated_habit')
-    if bool(reward) + bool(associated_habit) != 1:
-        raise ValidationError("В привычке должны указываться либо связанная привычка, либо вознаграждение")
+    is_pleasant = value.get('is_pleasant')
+    fields_presence = bool(reward) + bool(associated_habit)
+    if is_pleasant and fields_presence != 0:
+        raise ValidationError("В приятной привычке не должны быть заполнены поля associated_habit и reward")
+    if not is_pleasant and fields_presence != 1:
+        raise ValidationError("В полезной привычке должны быть заполнены либо поле associated_habit, либо поле reward")
 
 
 def habit_time_check(habit_time):
