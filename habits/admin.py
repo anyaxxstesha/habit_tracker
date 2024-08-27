9a7035fb9fa179eb4eccee13c071581dd11462da
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-08-27 14:53:27 +0300

    register habit model

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from habits.models import Habit
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'performance_time', 'is_pleasant', 'associated_habit', 'frequency', 'is_public')
+    list_filter = ('is_pleasant', 'owner')
+    search_fields = ('owner',)
