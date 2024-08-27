5e10edb9266ac28a082833ac69c852a194c0d8e2
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-08-27 15:16:28 +0300

    fix bugs admin.py

--- habits/admin.py
+++ habits/admin.py
@@ -5,6 +5,7 @@ from habits.models import Habit
 
 @admin.register(Habit)
 class HabitAdmin(admin.ModelAdmin):
-    list_display = ('id', 'owner', 'performance_time', 'is_pleasant', 'associated_habit', 'frequency', 'is_public')
+    list_display = (
+    'id', 'owner', 'performance_time', 'is_pleasant', 'associated_habit', 'frequency', 'is_public', 'habit_time')
     list_filter = ('is_pleasant', 'owner')
     search_fields = ('owner',)
