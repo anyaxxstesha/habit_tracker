beb3b686b71d54def17e6239a52e5e1b1a95ee8b
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-08-27 16:25:14 +0300

    add validation for habit_time

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 
 from habits.models import Habit
-from habits.validators import pleasant_habit_or_reward
+from habits.validators import pleasant_habit_or_reward, habit_time_check
 
 
 class HabitSerializer(serializers.ModelSerializer):
+    habit_time = serializers.DurationField(validators=[habit_time_check])
     class Meta:
         model = Habit
         fields = '__all__'
--- habits/validators.py
+++ habits/validators.py
@@ -1,8 +1,17 @@
+from datetime import timedelta
+
 from rest_framework.serializers import ValidationError
 
 
 def pleasant_habit_or_reward(value):
+    """Проверяет, что в модели заполнено либо поле associated_habit, либо reward"""
     reward = value.get('reward')
     associated_habit = value.get('associated_habit')
     if bool(reward) + bool(associated_habit) != 1:
         raise ValidationError("В привычке должны указываться либо связанная привычка, либо вознаграждение")
+
+
+def habit_time_check(habit_time):
+    """Проверяет, что в модели habit_time не больше 120 секунд"""
+    if habit_time > timedelta(minutes=2):
+        raise ValidationError("Время выполнения привычки не должно быть больше 120 секунд")
