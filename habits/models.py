3ad59714d828071425d53bc20313e73270e405dc
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-08-27 14:54:09 +0300

    modify habit descriptive fields

--- habits/models.py
+++ habits/models.py
@@ -14,8 +14,8 @@ class Habit(models.Model):
                               related_name='course', help_text='Укажите создателя привычки')
     place = models.TextField(verbose_name='Место для выполнения привычки',
                              help_text='Укажите место для выполнения привычки')
-    performance_time = models.TimeField(verbose_name='Время выполнения привычки',
-                                        help_text='Укажите время выполнения привычки')
+    performance_time = models.TimeField(verbose_name='Время, когда необходимо выполнять привычку',
+                                        help_text='Укажите время, когда необходимо выполнять привычку')
     action = models.TextField(verbose_name='Действие, которое представляет собой привычка',
                               help_text='Укажите действие, которое представляет собой привычка')
     is_pleasant = models.BooleanField(blank=True, null=True, default=False, verbose_name='Флаг приятной привычки',
@@ -23,8 +23,8 @@ class Habit(models.Model):
     associated_habit = models.ForeignKey('Habit', blank=True, null=True, verbose_name='Связанная привычка',
                                          help_text='Укажите связанную привычку', related_name='useful_habit',
                                          on_delete=models.SET_NULL)
-    frequency = models.DurationField(default=timedelta(days=1), verbose_name='Время на выполнение привычки',
-                                     help_text='Укажите время на выполнение привычки')
+    frequency = models.DurationField(default=timedelta(days=1), verbose_name='Периодичность выполнения привычки в днях',
+                                     help_text='Укажите периодичность выполнения привычки в днях')
     is_public = models.BooleanField(blank=True, null=True, default=False, verbose_name='Флаг публичной привычки',
                                     help_text='Укажите, является ли привычка публичной')
 
