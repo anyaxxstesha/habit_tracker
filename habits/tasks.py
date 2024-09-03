0d70587679cfc57b2900425161bb258e8200592a
Anna Tarazanova <an.tarazanova2016@yandex.ru>
2024-09-03 13:55:42 +0300

    add celery task

---
+++ habits/tasks.py
@@ -0,0 +1,15 @@
+from celery import shared_task
+from django.db.models import F
+from django.utils import timezone
+
+from habits.models import Habit
+from habits.services import start_notification
+
+
+@shared_task
+def send_notification_celery(course_id):
+    """Отправляет напоминание в Telegram для выполнения привычки"""
+    now = timezone.now()
+    queryset = Habit.objects.filter(perform_at__lt=now).prefetch_related('user', 'related_habit')
+    start_notification(habits=queryset)
+    queryset.update(next_perform_at=F('next_perform_at') + F('frequency'))
